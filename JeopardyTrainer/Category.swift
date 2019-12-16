4197cbecac7f6eed6473fb59301d31f6ed0cbaa1
Sandeep Singh <singhsz@bc.edu>
2019-12-16 15:36:10 -0500

    Final Exam

---
+++ JeopardyTrainer/Category.swift
@@ -0,0 +1,26 @@
+//
+//  Category.swift
+//  JeopardyTrainer
+//
+//  Created by Sandeep Singh on 12/16/19.
+//  Copyright © 2019 Sandeep Singh. All rights reserved.
+//
+
+import Foundation
+
+
+class Category {
+    var id: Int
+    var title: String
+    var cluesCount: Int
+
+    init(id: Int, title: String, cluesCount: Int) {
+        self.id = id
+        self.title = title
+        self.cluesCount = cluesCount
+    }
+    
+//    var id = 0
+//    var title = ""
+//    var cluesCount = 0
+}
---
+++ JeopardyTrainer/Clue.swift
@@ -0,0 +1,17 @@
+//
+//  Clue.swift
+//  JeopardyTrainer
+//
+//  Created by Sandeep Singh on 12/16/19.
+//  Copyright © 2019 Sandeep Singh. All rights reserved.
+//
+
+import Foundation
+
+class Clue {
+    var question = ""
+    var answer = ""
+    var categoryID = 0
+    var value = 0
+    var clueID = 0
+}
