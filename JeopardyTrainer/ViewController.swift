f7a1182aad0c7a9df7f22cb7df4df99f3ddcfd29
Sandeep Singh <singhsz@bc.edu>
2019-12-16 02:10:21 -0500

    Initial Commit

---
+++ JeopardyTrainer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JeopardyTrainer
+//
+//  Created by Sandeep Singh on 12/16/19.
+//  Copyright © 2019 Sandeep Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
